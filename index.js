dc8e34d548a44a41952860594620219494734821
sujith0408 <51961967+sujith0408@users.noreply.github.com>
2024-06-22 11:28:29 +0000

    My first action is ready

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from'@actions/core';
+import * as github from '@actions/github';
+
 try {
     const name = core.getInput('name');
     const output_value = `Hello ${name}!`;
-
     core.setOutput('Greeting', output_value);
 } catch (error) {
     core.setFailed(error.message);
